fdca67d00d33c22c76d808279a0654d113a73e9e
Ju Chanhyoung <ju031230321@gmail.com>
2024-04-26 20:01:03 +0900

    Update QEMU run script to use HDD

--- run_qemu.bat
+++ run_qemu.bat
@@ -2,4 +2,4 @@
 
 set qemu="C:\Program Files\qemu\qemu-system-x86_64.exe"
 
-%qemu% -L . -m 64M -M pc -blockdev driver=file,node-name=f0,filename=./Disk.img -device floppy,drive=f0 %1 %2 %3
+%qemu% -L . -m 64M -M pc -drive file=./HDD.img,format=raw,index=0,media=disk -blockdev driver=file,node-name=f0,filename=./Disk.img -device floppy,drive=f0 %1 %2 %3
--- run_qemu.sh
+++ run_qemu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 qemu-system-x86_64 \
-    -L . -m 64 -M pc \
+    -L . -m 64M -M pc \
+    -drive file=./HDD.img,format=raw,index=0,media=disk \
     -blockdev driver=file,node-name=f0,filename=./Disk.img \
     -device floppy,drive=f0 $1 $2 $3
